49803d0f8968cf84aad53b38ae546fc0a8c82ac2
CarlosDraftx <38110152+CarlosDraftx@users.noreply.github.com>
2018-07-15 15:16:06 -0400

    Update ICargarTrabajos.java

--- src/Vista/ICargarTrabajos.java
+++ src/Vista/ICargarTrabajos.java
@@ -121,7 +121,7 @@ public class ICargarTrabajos extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void desplegar_msj(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_desplegar_msj
-        int T = CTRL.i_copiar_archivo_trab();
+        int T = CTRL.i_copiar_archivo_trab(jTextField1.getText());
         if(T>0) {
             Msj.setText(T+" trabajos cargados con exito");
         } else {
