900e0db9cc0df6bbfaf02a2b358ad0bd99f6fcb0
Sagnior <38483140+Sagnior@users.noreply.github.com>
2018-07-15 21:28:31 -0400

    Add files via upload

--- src/Modelo/ReporteProfesores.java
+++ src/Modelo/ReporteProfesores.java
@@ -1,33 +1,60 @@
 package Modelo;
 
+import Controlador.Ctrl_Reportes;
 import java.util.ArrayList;
 import java.text.ParseException;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class ReporteProfesores extends Reporte {
-    private boolean ord;
-    
+    //Instancia de Ctrl_Reportes
+    private Ctrl_Reportes CTRL = Ctrl_Reportes.getInstance();
+    //-------------------------------
+    private int ord;
     private int Tamano; //para el size de ListaProf
     
     //----------Constructor-------------------------
     public ReporteProfesores()throws ParseException{ 
         super();
-        ord = false;
+        ord = 1;
     }
     //-----------Metodos---------------------------
-      public void ordenar(boolean ord){
+    public void ordenar(int ord){
         Tamano = ListaProf.size();
         Profesor aux = new Profesor();
-        if(ord){ //numero de trabajos
-            // esta funcion mejor se la pasamos a la clase control, hay q usar un atributo numtrab
+        if(ord == 1){ //numero de trabajos
+            System.out.println("entro a orden por numero de trabajos");
+            for(int i = Tamano-1; i > 0 ; i--){      
+                for(int j=0; j < i ; j++){
+                    System.out.println("Comparando "+ListaProf.get(j).getCantTrab()+" > "+ListaProf.get(j+1).getCantTrab());
+                    if(ListaProf.get(j).getCantTrab() > ListaProf.get(j+1).getCantTrab()){ 
+                        System.out.println("es mayor los cambio");   
+                            System.out.println(ListaProf.get(j+1).getApellido());
+                            aux.setApellido(ListaProf.get(j).getApellido());
+                            aux.setNombre(ListaProf.get(j).getNombre());
+                            aux.setCentro(ListaProf.get(j).getCentro());
+                            aux.setci(ListaProf.get(j).getCi());
+                            
+                            ListaProf.get(j).setApellido(ListaProf.get(j+1).getApellido());
+                            ListaProf.get(j).setNombre(ListaProf.get(j+1).getNombre());
+                            ListaProf.get(j).setCentro(ListaProf.get(j+1).getCentro());
+                            ListaProf.get(j).setci(ListaProf.get(j+1).getCi());
+
+                            ListaProf.get(j+1).setApellido(aux.getApellido());
+                            System.out.println(ListaProf.get(j+1).getApellido());
+                            ListaProf.get(j+1).setNombre(aux.getNombre());
+                            ListaProf.get(j+1).setCentro(aux.getCentro());
+                            ListaProf.get(j+1).setci(aux.getCi());
+                    }
+                }
+            }
         }
-        else{      //ordenamiento Apellido-Nombre--------------------------------------------------
+        else{      //ordenamiento Apellido-Nombre ord=0--------------------------------------------------
             for(int i = Tamano -1; i > 0 ; i--){      
                 for(int j = 0; j < i ; j++){
-                    if(ListaProf.get(j).getApellido().compareTo(ListaProf.get(j+1).getApellido())==0){ 
+                    if(ListaProf.get(j).getApellido().compareTo(ListaProf.get(j+1).getApellido())==0){ //si tienen mismo apellido
 
-                        if(ListaProf.get(j).getNombre().compareTo(ListaProf.get(j+1).getNombre())<0){ //pasa al nombre
+                        if(ListaProf.get(j).getNombre().compareTo(ListaProf.get(j+1).getNombre())>0){ //pasa al nombre
                             aux.setApellido(ListaProf.get(j).getApellido());
                             aux.setNombre(ListaProf.get(j).getNombre());
                             aux.setCentro(ListaProf.get(j).getCentro());
@@ -44,7 +71,7 @@ public class ReporteProfesores extends Reporte {
                             ListaProf.get(j+1).setci(aux.getCi());
                         }
                     }
-                    if(ListaProf.get(j).getApellido().compareTo(ListaProf.get(j+1).getApellido()) < 0){
+                    if(ListaProf.get(j).getApellido().compareTo(ListaProf.get(j+1).getApellido()) > 0){
 
                         aux.setApellido(ListaProf.get(j).getApellido());
                         aux.setNombre(ListaProf.get(j).getNombre());
@@ -72,5 +99,11 @@ public class ReporteProfesores extends Reporte {
                 return true;            
         }
         return false;
+    }
+    
+    ConjuntoProfesores CP= CTRL.get_Inst_CP();
+    
+    public void listarProf(Profesor a){
+        ListaProf.add(a);
     }
 }
